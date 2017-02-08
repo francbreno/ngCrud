a5d10656621487aadfb4db3f76cef6734f6691ef
Breno Soares <francbreno@gmail.com>
2017-02-08 15:23:28 -0300

    estrutura básica da app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    UsersComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
